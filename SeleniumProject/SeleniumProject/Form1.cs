beaabcfb28dea4b473ffd5f5d144b0cc8e1674a0
nhan <tonhanlk7103@gmail.com>
2023-11-13 15:09:52 +0700

    thuc hanh 13/11

---
+++ SeleniumProject/SeleniumProject/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "http://google.com.vn";
+            driver.Navigate();
+        }
+    }
+}
