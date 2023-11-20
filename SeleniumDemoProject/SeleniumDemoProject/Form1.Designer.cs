7e4916449deeda73f48cdb5cc3647ea2d42e8e85
nhan <tonhanlk7103@gmail.com>
2023-11-20 15:11:39 +0700

    Thuc hanh buoi 6 20/11

---
+++ SeleniumDemoProject/SeleniumDemoProject/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿
+namespace SeleniumDemoProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txB1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txB1
+            // 
+            this.txB1.Location = new System.Drawing.Point(225, 135);
+            this.txB1.Multiline = true;
+            this.txB1.Name = "txB1";
+            this.txB1.Size = new System.Drawing.Size(225, 40);
+            this.txB1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(256, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(148, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Trọng Nhân - 85";
+            // 
+            // btn1
+            // 
+            this.btn1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1.Location = new System.Drawing.Point(260, 230);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(108, 37);
+            this.btn1.TabIndex = 2;
+            this.btn1.Text = "Open Url";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txB1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txB1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn1;
+    }
+}
+
---
+++ SeleniumDemoProject/SeleniumDemoProject/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System.Threading;
+
+namespace SeleniumDemoProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            IWebDriver driver = new ChromeDriver();
+            string url = txB1.Text.ToString();
+            if(url == "")
+                driver.Navigate().GoToUrl("http://vnexpress.net"); //trong nhan - 85
+            else
+                driver.Navigate().GoToUrl(url);
+
+
+            //driver.Close();
+
+            //tat man hinh den
+            ChromeDriverService chrome = ChromeDriverService.CreateDefaultService();
+            chrome.HideCommandPromptWindow = true;
+            //dieu huong trinh duyet
+            IWebDriver driver1 = new ChromeDriver(chrome);
+
+            //dong moi cua so lien ket
+            //driver.Quit();
+
+            //lay url 
+            String link_hientai = driver.Url;
+            Console.WriteLine(link_hientai);
+
+            //lay title
+            string urlTitle = driver.Title;
+            Console.WriteLine(urlTitle);
+            txB1.Text = urlTitle.ToString();
+
+            //lay pagesource
+            String pageSource = driver.PageSource;
+            Console.WriteLine(pageSource);
+
+            //dieu huong trang
+            driver.Navigate().GoToUrl("https://ou.edu.vn/");
+            Thread.Sleep(2000);
+            driver.Navigate().Back();
+            Thread.Sleep(2000);
+            driver.Navigate().Forward();// 85 - to trong nhan - nay gio em quen comment ten voi stt
+
+        }
+    }
+}
