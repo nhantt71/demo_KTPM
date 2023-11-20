700c6c9d713e1cf0f1c02115d39a4ea87ae005b7
nhan <nhanlk7103@gmail.com>
2023-11-20 23:02:43 +0700

    Done 20/11

---
+++ SeleniumB6/SeleniumB6/Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace SeleniumB6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.bt4 = new System.Windows.Forms.Button();
+            this.txt3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.bt5 = new System.Windows.Forms.Button();
+            this.bt6 = new System.Windows.Forms.Button();
+            this.txt4 = new System.Windows.Forms.TextBox();
+            this.txt5 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(172, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(161, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "85_Trọng Nhân";
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(108, 138);
+            this.txt1.Multiline = true;
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(244, 35);
+            this.txt1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(48, 144);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "URL";
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(87, 258);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(108, 47);
+            this.btn1.TabIndex = 3;
+            this.btn1.Text = "Open Url";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(275, 258);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(108, 47);
+            this.btn2.TabIndex = 4;
+            this.btn2.Text = "Close";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(458, 258);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(108, 47);
+            this.btn3.TabIndex = 5;
+            this.btn3.Text = "Quit";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(524, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 25);
+            this.label3.TabIndex = 6;
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(508, 138);
+            this.txt2.Multiline = true;
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(250, 35);
+            this.txt2.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(409, 141);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 25);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Display:";
+            // 
+            // bt4
+            // 
+            this.bt4.Location = new System.Drawing.Point(650, 258);
+            this.bt4.Name = "bt4";
+            this.bt4.Size = new System.Drawing.Size(108, 47);
+            this.bt4.TabIndex = 9;
+            this.bt4.Text = "Get Title";
+            this.bt4.UseVisualStyleBackColor = true;
+            this.bt4.Click += new System.EventHandler(this.bt4_Click);
+            // 
+            // txt3
+            // 
+            this.txt3.Location = new System.Drawing.Point(327, 357);
+            this.txt3.Name = "txt3";
+            this.txt3.Size = new System.Drawing.Size(141, 31);
+            this.txt3.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(242, 357);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 25);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "key";
+            // 
+            // bt5
+            // 
+            this.bt5.Location = new System.Drawing.Point(318, 405);
+            this.bt5.Name = "bt5";
+            this.bt5.Size = new System.Drawing.Size(150, 48);
+            this.bt5.TabIndex = 12;
+            this.bt5.Text = "send key";
+            this.bt5.UseVisualStyleBackColor = true;
+            this.bt5.Click += new System.EventHandler(this.bt5_Click);
+            // 
+            // bt6
+            // 
+            this.bt6.Location = new System.Drawing.Point(578, 387);
+            this.bt6.Name = "bt6";
+            this.bt6.Size = new System.Drawing.Size(155, 66);
+            this.bt6.TabIndex = 13;
+            this.bt6.Text = "Bai6";
+            this.bt6.UseVisualStyleBackColor = true;
+            this.bt6.Click += new System.EventHandler(this.bt6_Click);
+            // 
+            // txt4
+            // 
+            this.txt4.Location = new System.Drawing.Point(508, 327);
+            this.txt4.Multiline = true;
+            this.txt4.Name = "txt4";
+            this.txt4.Size = new System.Drawing.Size(145, 37);
+            this.txt4.TabIndex = 14;
+            // 
+            // txt5
+            // 
+            this.txt5.Location = new System.Drawing.Point(680, 327);
+            this.txt5.Multiline = true;
+            this.txt5.Name = "txt5";
+            this.txt5.Size = new System.Drawing.Size(145, 37);
+            this.txt5.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(837, 465);
+            this.Controls.Add(this.txt5);
+            this.Controls.Add(this.txt4);
+            this.Controls.Add(this.bt6);
+            this.Controls.Add(this.bt5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txt3);
+            this.Controls.Add(this.bt4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button bt4;
+        private System.Windows.Forms.TextBox txt3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button bt5;
+        private System.Windows.Forms.Button bt6;
+        private System.Windows.Forms.TextBox txt4;
+        private System.Windows.Forms.TextBox txt5;
+    }
+}
+
---
+++ SeleniumB6/SeleniumB6/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System.Threading;
+
+namespace SeleniumB6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public string defaultUrl = "http://vnexpress.net/";
+
+        // 85_TrongNhan
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            // open url by 85_TrongNhan
+            IWebDriver driver = new ChromeDriver();
+            string url = txt1.Text.ToString();
+            if (url == "" ) { driver.Navigate().GoToUrl(defaultUrl); }
+            else {  driver.Navigate().GoToUrl(url);}
+
+            // get url by 85_TrongNhan
+            String link_hentai = driver.Url;
+            Console.WriteLine(link_hentai);
+
+            // get page source 85_TrongNhan
+            String PageSource = driver.PageSource;
+            Console.WriteLine(PageSource);
+
+            // close command window by 85_TrongNhan
+            ChromeDriverService chrome = ChromeDriverService.CreateDefaultService();
+            chrome.HideCommandPromptWindow = true;
+
+            // direct path of web by 85_TrongNhan
+            driver.Navigate().GoToUrl("http://google.com");
+            Thread.Sleep(1000);
+            driver.Navigate().Back();
+            Thread.Sleep(1000);
+            driver.Navigate().Refresh();
+
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            IWebDriver driver = new ChromeDriver();
+            // close web by 85_TrongNhan
+            driver.Close();
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            IWebDriver driver = new ChromeDriver();
+            // quit web by 85_TrongNhan
+            driver.Quit();
+        }
+
+        private void bt4_Click(object sender, EventArgs e)
+        {
+            IWebDriver driver = new ChromeDriver();
+            // get Title then display in display textbox by 85_TrongNhan
+            string urlTitle = driver.Title;
+            txt2.Text = urlTitle.ToString();
+        }
+
+        private void bt5_Click(object sender, EventArgs e)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.google.com/");
+            // send key by 85_TrongNhan
+            String url = txt3.Text;
+            IWebElement element = driver.FindElement(By.Name("q"));
+            element.SendKeys(url.ToString());
+        }
+
+        private void bt6_Click(object sender, EventArgs e)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.facebook.com/");
+            // send key input account password by 85_TrongNhan
+            String acc = txt4.Text;
+            String pass = txt5.Text;
+            IWebElement e_email = driver.FindElement(By.Name("email"));
+            IWebElement passW = driver.FindElement(By.Name("pass"));
+            IWebElement e_login = driver.FindElement(By.Name("login"));
+            e_email.SendKeys(acc.ToString());
+            passW.SendKeys(pass.ToString());
+            e_login.Click();
+        }
+    }
+}
